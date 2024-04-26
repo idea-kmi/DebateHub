d082e6e82437e3dcd21519f0d7970cdfb484ef9d
Samantha.Colclough <samantha.colclough@open.ac.uk>
2024-04-26 08:48:01 +0100

    adding to blocklist

--- core/email-blocklist.php
+++ core/email-blocklist.php
@@ -4,5 +4,8 @@ return array(
     'slippmail.com',
     'mailwebber.com',
     'dig.oueue.com',
+    'farironalds.com',
+    'yandex.ru',
+    'dig.oueue.com',
     // Add more domains as necessary
 );

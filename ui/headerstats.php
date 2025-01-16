d42cf6066cf3f893cef9b1198b5861eb6dc0932a
Michelle S Bachler <michelle.bachler@open.ac.uk>
2025-01-16 12:22:39 +0000

    Make cookie banner links absolute

--- ui/headerstats.php
+++ ui/headerstats.php
@@ -154,7 +154,7 @@
 					<button type="button" class="cookieConsentButton" data-bs-dismiss="alert" aria-label="No" id="declineAnlyticsCookies">No</button>
 				</div>
 				<br/>
-				<a href="ui/pages/cookies.php">Read our cookie policy</a>
+				<a href="<?php print($CFG->homeAddress);?>ui/pages/cookies.php">Read our cookie policy</a>
 			</div>
 		</div>
 		<div id="maincenter" class="container-fluid">
